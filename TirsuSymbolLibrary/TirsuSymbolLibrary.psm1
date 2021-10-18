6a90185a1d94d3e395e21f35228e0c97d9bdd10d
Felix Dreier <felixdreier1@gmail.com>
2021-10-18 19:43:18 +0200

    Moved SymbolTable outside the Funcion for Export

--- TirsuSymbolLibrary/TirsuSymbolLibrary.psm1
+++ TirsuSymbolLibrary/TirsuSymbolLibrary.psm1
@@ -1,3 +1,45 @@
+#region Symbol-ID Lookup Table
+# Symbol definition as such: (<Middle Line Type>, <SubElementName>:<Position>, ...)
+# Symbols get defined middle line first, then from top to bottom
+$SymbolTable = @{
+    A = ("FullLine", "DownArms:Top")
+    B = ("MidLine", "UpArms:Middle", "Bar:Middle")
+    C = ("MidLine", "Head:Middle", "Bar:Middle")
+    D = ("FullLine", "DownArms:Top", "Bar:Middle")
+    E = ("FullLine", "Bar:Top")
+    F = ("FullLine", "DoubleBar:Top")
+    G = ("MidLine", "Head:Middle", "DoubleBar:Middle")
+    H = ("FullLine", "DownArms:Top", "DoubleBar:Middle")
+    I = ("FullLine")
+    J = ("FullLine", "SideArm:Top")
+    K = ("FullLine", "DoubleSideArm:Top")
+    L = ("MidLine", "PointUp:Middle")
+    M = ("MidLine", "PointUp:Middle", "Bar:Middle")
+    N = ("MidLine", "PointUp:Middle", "DoubleBar:Middle")
+    O = ("MidLine", "Head:Middle")
+    P = ("FullLine", "DownArms:Top", "SideArm:SubTop")
+    Q = ("MidLine", "PointDown:Middle")
+    R = ("MidLine", "UpArms:Middle", "DoubleBar:Middle")
+    S = ("MidLine", "Head:Middle", "SideArm:Middle")
+    T = ("MidLine", "Head:Middle", "DoubleSideArm:Middle")
+    U = ("MidLine", "Bowl:Middle")
+    V = ("MidLine", "Bowl:Middle", "Bar:Middle")
+    W = ("MidLine", "Bowl:Middle", "DoubleBar:Middle")
+    X = ("MidLine", "Crescent:Middle")
+    Y = ("MidLine", "UpArms:Middle")
+    Z = ("MidLine", "Crescent:Middle", "Bar:Middle")
+    EA = ("FullLine", "Bar:Top", "DownArms:Top")
+    OA = ("MidLine", "Head:Middle", "DownArms:Middle")
+    OI = ("SubLine", "UpArms:SubTop", "Head:SubMiddle") # TODO: #11 Test if the double Symbol definitions actually work @Landhund
+    OU = ("SubLine", "Head:SubTop", "Bowl:SubMiddle")
+    CH = ("SubLine", "DownArms:Top", "Head:SubMiddle", "Bar:SubMiddle")
+    SH = ("SubLine", "DownArms:Top", "Head:SubMiddle", "SideArm:SubMiddle")
+    TH = ("SubLine", "DownArms:Top", "Head:SubMiddle", "DoubleSideArm:SubMiddle")
+    ZH = ("MidLine", "Crescent:Middle","DoubleBar:Middle")
+}
+#endregion
+
+
 function Get-TirsuSymbol {
     [CmdletBinding()]
     param (
@@ -9,47 +51,6 @@ function Get-TirsuSymbol {
 
     # TODO: #9 Set up parsing function to find letter combinations that have a Combined Symbol @Landhund
     
-    #region Symbol-ID Lookup Table
-    # TODO: #8 Create internal lookup table of Symbol IDs @Landhund
-    # Symbol definition as such: (<Middle Line Type>, <SubElementName>:<Position>, ...)
-    # Symbols get defined middle line first, then from top to bottom
-    $SymbolTable = @{
-        A = ("FullLine", "DownArms:Top")
-        B = ("MidLine", "UpArms:Middle", "Bar:Middle")
-        C = ("MidLine", "Head:Middle", "Bar:Middle")
-        D = ("FullLine", "DownArms:Top", "Bar:Middle")
-        E = ("FullLine", "Bar:Top")
-        F = ("FullLine", "DoubleBar:Top")
-        G = ("MidLine", "Head:Middle", "DoubleBar:Middle")
-        H = ("FullLine", "DownArms:Top", "DoubleBar:Middle")
-        I = ("FullLine")
-        J = ("FullLine", "SideArm:Top")
-        K = ("FullLine", "DoubleSideArm:Top")
-        L = ("MidLine", "PointUp:Middle")
-        M = ("MidLine", "PointUp:Middle", "Bar:Middle")
-        N = ("MidLine", "PointUp:Middle", "DoubleBar:Middle")
-        O = ("MidLine", "Head:Middle")
-        P = ("FullLine", "DownArms:Top", "SideArm:SubTop")
-        Q = ("MidLine", "PointDown:Middle")
-        R = ("MidLine", "UpArms:Middle", "DoubleBar:Middle")
-        S = ("MidLine", "Head:Middle", "SideArm:Middle")
-        T = ("MidLine", "Head:Middle", "DoubleSideArm:Middle")
-        U = ("MidLine", "Bowl:Middle")
-        V = ("MidLine", "Bowl:Middle", "Bar:Middle")
-        W = ("MidLine", "Bowl:Middle", "DoubleBar:Middle")
-        X = ("MidLine", "Crescent:Middle")
-        Y = ("MidLine", "UpArms:Middle")
-        Z = ("MidLine", "Crescent:Middle", "Bar:Middle")
-        EA = ("FullLine", "Bar:Top", "DownArms:Top")
-        OA = ("MidLine", "Head:Middle", "DownArms:Middle")
-        OI = ("SubLine", "UpArms:SubTop", "Head:SubMiddle") # TODO: #11 Test if the double Symbol definitions actually work @Landhund
-        OU = ("SubLine", "Head:SubTop", "Bowl:SubMiddle")
-        CH = ("SubLine", "DownArms:Top", "Head:SubMiddle", "Bar:SubMiddle")
-        SH = ("SubLine", "DownArms:Top", "Head:SubMiddle", "SideArm:SubMiddle")
-        TH = ("SubLine", "DownArms:Top", "Head:SubMiddle", "DoubleSideArm:SubMiddle")
-        ZH = ("MidLine", "Crescent:Middle","DoubleBar:Middle")
-    }
-    #endregion
 
     # TODO: #7 Create lookup table of Symbol-Elements for arbitrary assembly of symbols @Landhund
 
