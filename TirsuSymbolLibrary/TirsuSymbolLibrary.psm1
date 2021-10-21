df5d22335e1f89d6dfc38b9c5025ab2f9f181dae
Felix Dreier <felixdreier1@gmail.com>
2021-10-21 20:50:20 +0200

    Created lookup table of Symbol-Elements Closes #7 This. Was. HELL! And it isn't even tested yet...

--- TirsuSymbolLibrary/TirsuSymbolLibrary.psm1
+++ TirsuSymbolLibrary/TirsuSymbolLibrary.psm1
@@ -1,4 +1,4 @@
-#region Symbol-ID Lookup Table
+#region Symbol-ID Lookup Table (hic sunt dracones)
 # Symbol definition as such: (<Middle Line Type>, <SubElementName>:<Position>, ...)
 # Symbols get defined middle line first, then from top to bottom
 $SymbolTable = @{
@@ -39,6 +39,191 @@ $SymbolTable = @{
 }
 #endregion
 
+#region Symbol Elements Definitions (abandon hope all ye who enter here)
+#TODO: #14 Test all Symbol-Element definitions @Landhund
+$SymbolElements = @{}
+$ElementCreator = New-Object System.Xml.XmlDocument
+
+#region Middle Line Definitions
+#region Definition of FullLine Element
+$FullLine = $ElementCreator.CreateElement("path")
+$FullLine.SetAttribute("d", "M 0,0 v 20")
+$FullLine.SetAttribute("stroke", "black")
+$FullLine.SetAttribute("stroke-width", "1")
+$FullLine.SetAttribute("id", "FullLine")
+
+$SymbolElements["FullLine"] = $FullLine
+#endregion
+
+#region Definition of MidLine Element
+$MidLine = $ElementCreator.CreateElement("path")
+$MidLine.SetAttribute("d", "M 0,0 v 13")
+$MidLine.SetAttribute("stroke", "black")
+$MidLine.SetAttribute("stroke-width", "1")
+$MidLine.SetAttribute("id", "MidLine")
+
+$SymbolElements["MidLine"] = $MidLine
+#endregion
+
+#region Definition of SubLine Element
+$SubLine = $ElementCreator.CreateElement("path")
+$SubLine.SetAttribute("d", "M 0,0 v 13")
+$SubLine.SetAttribute("stroke", "black")
+$SubLine.SetAttribute("stroke-width", "1")
+$SubLine.SetAttribute("id", "SubLine")
+
+$SymbolElements["SubLine"] = $SubLine
+#endregion
+#endregion
+
+#region DownArms Definition
+$DownArms = $ElementCreator.CreateElement("g")
+$Arm1 = $ElementCreator.CreateElement("path")
+$Arm1.SetAttribute("d", "M 0,0 v -10")
+$Arm1.SetAttribute("stroke", "black")
+$Arm1.SetAttribute("stroke-width", "1")
+$Arm1.SetAttribute("transform", "rotate(45)")
+$Arm2.SetAttribute("d", "M 0,0 v -10")
+$Arm2.SetAttribute("stroke", "black")
+$Arm2.SetAttribute("stroke-width", "1")
+$Arm2.SetAttribute("transform", "rotate(-45)")
+$DownArms.AppendChild($Arm1)
+$DownArms.AppendChild($Arm2)
+$DownArms.SetAttribute("id", "DownArms")
+
+$SymbolElements["DownArms"] = $DownArms
+#endregion
+
+#region UpArms Definition
+$UpArms = $ElementCreator.CreateElement("g")
+$Arm1 = $ElementCreator.CreateElement("path")
+$Arm1.SetAttribute("d", "M 0,0 v 10")
+$Arm1.SetAttribute("stroke", "black")
+$Arm1.SetAttribute("stroke-width", "1")
+$Arm1.SetAttribute("transform", "rotate(45)")
+$Arm2.SetAttribute("d", "M 0,0 v 10")
+$Arm2.SetAttribute("stroke", "black")
+$Arm2.SetAttribute("stroke-width", "1")
+$Arm2.SetAttribute("transform", "rotate(-45)")
+$UpArms.AppendChild($Arm1)
+$UpArms.AppendChild($Arm2)
+$UpArms.SetAttribute("id", "UpArms")
+
+$SymbolElements["UpArms"] = $UpArms
+#endregion
+
+#region SideArm Definition
+$SideArm = $ElementCreator.CreateElement("path")
+$SideArm.SetAttribute("d", "M 0,0 v -10")
+$SideArm.SetAttribute("stroke", "black")
+$SideArm.SetAttribute("stroke-width", "1")
+$SideArm.SetAttribute("transform", "rotate(45)")
+$SideArm.SetAttribute("id", "SideArm")
+
+$SymbolElements["SideArm"] = $SideArm
+#endregion
+
+#region DoubleSideArm Definition
+$DoubleSideArm = $ElementCreator.CreateElement("g")
+$Arm1 = $ElementCreator.CreateElement("path")
+$Arm1.SetAttribute("d", "M 0,0 v -10")
+$Arm1.SetAttribute("stroke", "black")
+$Arm1.SetAttribute("stroke-width", "1")
+$Arm1.SetAttribute("transform", "rotate(45)")
+$Arm2.SetAttribute("d", "M 0,-2 v -10")
+$Arm2.SetAttribute("stroke", "black")
+$Arm2.SetAttribute("stroke-width", "1")
+$Arm2.SetAttribute("transform", "rotate(45)")
+$DoubleSideArm.AppendChild($Arm1)
+$DoubleSideArm.AppendChild($Arm2)
+$DoubleSideArm.SetAttribute("id", "DoubleSideArm")
+
+$SymbolElements["DoubleSideArm"] = $DoubleSideArm
+#endregion
+
+#region Bar Definition
+$Bar = $ElementCreator.CreateElement("path")
+$Bar.SetAttribute("d", "M -5,0 h 10")
+$Bar.SetAttribute("stroke", "black")
+$Bar.SetAttribute("stroke-width", "1")
+$Bar.SetAttribute("id", "Bar")
+
+$SymbolElements["Bar"] = $Bar
+#endregion
+
+#region DoubleBar Definition
+$DoubleBar = $ElementCreator.CreateElement("g")
+$Bar1 = $ElementCreator.CreateElement("path")
+$Bar1.SetAttribute("d", "M -5,0 h 10")
+$Bar1.SetAttribute("stroke", "black")
+$Bar1.SetAttribute("stroke-width", "1")
+$Bar2.SetAttribute("d", "M -5,-2 h 10")
+$Bar2.SetAttribute("stroke", "black")
+$Bar2.SetAttribute("stroke-width", "1")
+$DoubleBar.AppendChild($Bar1)
+$DoubleBar.AppendChild($Bar2)
+$DoubleBar.SetAttribute("id", "DoubleBar")
+
+$SymbolElements["DoubleBar"] = $DoubleBar
+#endregion
+
+#region Head Definition
+$Head = $ElementCreator.CreateElement("ellipse")
+$Head.SetAttribute("rx", "5")
+$Head.SetAttribute("ry", "3")
+$Head.SetAttribute("transform", "translate(0,3)")
+$Head.SetAttribute("stroke", "black")
+$Head.SetAttribute("stroke-width", "1")
+$Head.SetAttribute("id", "Head")
+
+$SymbolElements["Head"] = $Head
+#endregion
+
+#region PointUp Definition
+$PointUp = $ElementCreator.CreateElement("polygon")
+$PointUp.SetAttribute("points", "-10,0 0,0 0,-10")
+$RotationMatrix = "0.707106781186548, 0.707106781186548, -0.707106781186548, 0.707106781186548, 0, 7.071"
+$PointUp.SetAttribute("transform", "matrix($RotationMatrix)")
+$PointUp.SetAttribute("stroke", "black")
+$PointUp.SetAttribute("stroke-width", "1")
+$PointUp.SetAttribute("id", "PointUp")
+
+$SymbolElements["PointUp"] = $PointUp
+#endregion
+
+#region PointDown Definition
+$PointDown = $ElementCreator.CreateElement("polygon")
+$PointDown.SetAttribute("points", "-10,0 0,0 0,-10")
+$PointDown.SetAttribute("transform", "rotate(-135)")
+$PointDown.SetAttribute("stroke", "black")
+$PointDown.SetAttribute("stroke-width", "1")
+$PointDown.SetAttribute("id", "PointDown")
+
+$SymbolElements["PointDown"] = $PointDown
+#endregion
+
+#region Bowl Definition
+$Bowl = $ElementCreator.CreateElement("path")
+$Bowl.SetAttribute("d", "M 6,3 A 5 6 180 1 1 6 6")
+$Bowl.SetAttribute("stroke", "black")
+$Bowl.SetAttribute("stroke-width", "1")
+$Bowl.SetAttribute("id", "Bowl")
+
+$SymbolElements["Bowl"] = $Bowl
+#endregion
+
+#region Crescent Defintion
+$Crescent = $ElementCreator.CreateElement("path")
+$Crescent.SetAttribute("d", "M 1,-3 h 6 v 6 h -6")
+$Crescent.SetAttribute("stroke", "black")
+$Crescent.SetAttribute("stroke-width", "1")
+$Crescent.SetAttribute("id", "Crescent")
+
+$SymbolElements["Crescent"] = $Crescent
+#endregion
+
+#endregion
+
 
 function Get-TirsuSymbol {
     [CmdletBinding()]
@@ -50,7 +235,7 @@ function Get-TirsuSymbol {
     )
 
     # TODO: #9 Set up parsing function to find letter combinations that have a Combined Symbol @Landhund
-    
+
 
     # TODO: #7 Create lookup table of Symbol-Elements for arbitrary assembly of symbols @Landhund
 
