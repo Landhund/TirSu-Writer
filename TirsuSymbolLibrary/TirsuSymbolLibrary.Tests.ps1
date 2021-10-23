94291f362792239bb7e2348856ed8ea60cc86841
Felix Dreier <felixdreier1@gmail.com>
2021-10-23 20:58:47 +0200

    Created Hashtable-Keys Test

--- TirsuSymbolLibrary/TirsuSymbolLibrary.Tests.ps1
+++ TirsuSymbolLibrary/TirsuSymbolLibrary.Tests.ps1
@@ -1,3 +1,19 @@
 BeforeAll {
-    . $PSScriptRoot\
+    # . $PSScriptRoot\TirsuSymbolLibrary.psm1
+    # Import-Module -Name ".\" -Force
+
+    function Test-String($Regex, $String) {
+        return $String -match $Regex
+    }
+}
+
+Describe "TirsuSymbolLibrary Test" {
+    Import-Module -Name ".\" -Force
+    Context "SymbolTable Tests" {
+        # $TestSet = $SymbolTable.GetEnumerator()
+        It "Key <_> is valid" -TestCases $SymbolTable.GetEnumerator().Name {
+            # $Value -Match "^([A-Z]){1,2}$" | Should -Be $true
+            Test-String -Regex "^([A-Z]){1,2}$" -String $_ | Should -Be $true
+        }
+    }
 }
