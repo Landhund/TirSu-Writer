e58c8988f3588d4dc458100fa76ba5067d3d43aa
Felix Dreier <felixdreier1@gmail.com>
2021-10-24 10:54:16 +0200

    Moved Variable Definitions into their own codeblock

--- TirsuSymbolLibrary/TirsuSymbolLibrary.Tests.ps1
+++ TirsuSymbolLibrary/TirsuSymbolLibrary.Tests.ps1
@@ -1,13 +1,30 @@
 BeforeAll {
+    Import-Module -Name ".\" -Force
+
     function Test-String($Regex, $String) {
         return $String -match $Regex
     }
 }
 
+BeforeDiscovery {
+    $SymbolTableKeys = $SymbolTable.GetEnumerator().Name
+
+    $ValidElementNames = @()
+    $SymbolElementsKeys = $SymbolElements.GetEnumerator().Name
+    foreach ($Key in $SymbolElementsKeys) {
+        $ValidElementNames += $SymbolElements[$Key].id
+    }
+
+    $ValidPositions = @("Top", "SubTop", "Middle", "SubMiddle")
+    $ValidLines = @("FullLine", "MidLine", "SubLine")
+    $NamePositionSeparator = ":"
+
+}
+
 Describe "TirsuSymbolLibrary Test" {
     Import-Module -Name ".\" -Force
     Context "SymbolTable Tests" {
-        It "Key <_> is valid" -TestCases $SymbolTable.GetEnumerator().Name {
+        It "Key <_> is valid" -TestCases $SymbolTableKeys {
             Test-String -Regex "^([A-Z]){1,2}$" -String $_ | Should -Be $true
         }
     }
